e2c052469ee83130e2a842c674eefff0d311b47b
Sumeye <dalkilincsumeye@gmail.com>
2022-12-13 21:57:23 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectRestSApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectRestSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestSApplication.class, args);
+    }
+
+}
